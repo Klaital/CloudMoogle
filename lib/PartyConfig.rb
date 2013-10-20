25b8d28e4dbbcd069f54e0679d71ea179857d2a8
Chris Cox <chriscox@abandonedfactory.net>
2013-10-19 21:40:39 -0700

    Adding skeleton code for DB-based persistence

--- lib/PartyConfig.rb
+++ lib/PartyConfig.rb
@@ -1,18 +1,32 @@
 
 class PartyConfig
-    attr_accessor :player_characters
-    attr_accessor :start_time
-    attr_accessor :end_time
+  attr_reader :id
 
-    def initialize(player_characters=['Klaital', 'Demandred'], start_time=Time.now, end_time=Time.now + (60*60*12))
-        @player_characters = player_characters
-        @start_time = start_time
-        @end_time = end_time
-    end
+  attr_accessor :player_characters
+  attr_accessor :start_time
+  attr_accessor :end_time
 
-    # Add a new member to the party without allowing duplicates.
-    def add_player(new_party_member)
-        @player_characters << new_party_memeber unless(@player_characters.include?(new_party_member)
-    end
+  def initialize(player_characters=['Klaital', 'Demandred'], start_time=Time.now, end_time=Time.now + (60*60*12))
+    @player_characters = player_characters
+    @start_time = start_time
+    @end_time = end_time
+  end
+
+  # Add a new member to the party without allowing duplicates.
+  def add_player(new_party_member)
+    @player_characters << new_party_memeber unless(@player_characters.include?(new_party_member)
+  end
+
+  # Read the Party Configuration data from the database
+  def PartyConfig.load(id)
+    # TODO: choose a database, implement actual read logic here
+    p = PartyConfig.new
+  end
+
+  # Save the Party Configuration data back out to the database
+  def save
+    # TODO: implement actual database-writing logic here
+    return false
+  end
 end
 
