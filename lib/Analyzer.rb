59e4f42a8bce7c4b33e46dbc4e5eef11b3d70c73
Chris Cox <chriscox@abandonedfactory.net>
2013-10-19 21:40:22 -0700

    Adding S3 upload logic

--- lib/Analyzer.rb
+++ lib/Analyzer.rb
@@ -2,8 +2,6 @@ require 'aws-sdk'
 require '../lib/configs'
 require '../lib/PartyConfig'
 
-
-
 class Analyzer
   attr_accessor :party_id
   attr_reader :actions
@@ -14,7 +12,11 @@ class Analyzer
 
   def analyze_offense
     return false if (@party_id.nil?)
-    @party_config = PartyConfig.new(@party_id)
+    @party_config = PartyConfig.load(@party_id)
+    if (@party_config.nil?)
+      LOGGER.debug {"Unable to load party configuration for id #{@party_id}"}
+      return false
+    end
 
     @actions = fetch_actions # Pull the set of Actions performed by this party from the data store (probably a mongodb or dynamodb somewhere)
 
@@ -25,8 +27,14 @@ class Analyzer
 
     # Write the results to S3
     output_filename = "#{@party_id}.offense.xml"
-    # TODO: add actual S3 upload logic
+    upload_analysis(output_filename, output_data)
   end
 
-
+  def upload_analysis(filename, data)
+    bucket = CONFIGS[:aws][:s3][:analysis_bucket]
+    s3 = AWS::S3.new
+    b = s3.buckets[bucket]
+    o = b.objects[filename]
+    o.write(data)
+  end
 end
