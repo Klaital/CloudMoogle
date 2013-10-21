b73abf9a290fa31a13750171c4b4135676363217
Chris Cox <chriscox@abandonedfactory.net>
2013-10-20 18:13:25 -0700

    Adding MySQL connection logic for PartyConfig

--- lib/PartyConfig.rb
+++ lib/PartyConfig.rb
@@ -1,15 +1,16 @@
 
 class PartyConfig
-  attr_reader :id
+  attr_accessor :id
 
   attr_accessor :player_characters
-  attr_accessor :start_time
-  attr_accessor :end_time
+  attr_accessor :name, :start_time, :end_time
 
   def initialize(player_characters=['Klaital', 'Demandred'], start_time=Time.now, end_time=Time.now + (60*60*12))
+    @id = nil
     @player_characters = player_characters
     @start_time = start_time
     @end_time = end_time
+    @name = ''
   end
 
   # Add a new member to the party without allowing duplicates.
@@ -19,14 +20,27 @@ class PartyConfig
 
   # Read the Party Configuration data from the database
   def PartyConfig.load(id)
-    # TODO: choose a database, implement actual read logic here
+    return false unless(id.kind_of?(Fixnum))
     p = PartyConfig.new
+    p.id = id
+
+    res = PARTY_CONFIG_MYSQL.query("SELECT * party_configs WHERE party_config.id = #{id}")
+    return false if (res.nil? || res.length == 0)
+    row = res.fetch_row
+    return false if (row.nil? || row.length < 4)
+    p.start_time = row['start_time']
+    p.end_time = row['end_time']
+    p.name = row['name']
+
+    return p
   end
 
   # Save the Party Configuration data back out to the database
   def save
     # TODO: implement actual database-writing logic here
-    return false
+    id = @id.nil? ? 'NULL' : @id.to_s
+    res = PARTY_CONFIG_MYSQL.query("INSERT INTO party_configs VALUES (#{id},'#{@name}','#{@start_time}', '#{@end_time}') ON DUPLICATE KEY UPDATE")
+    return res.nil?
   end
 end
 
