94bb398c47f1aafa472adecabb49d3717d2e9de7
Chris Cox <chriscox@abandonedfactory.net>
2013-10-20 22:10:07 -0700

    Adding custom logging method.

--- lib/configs.rb
+++ lib/configs.rb
@@ -8,15 +8,40 @@ require 'aws-sdk'
 CREDS = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/../config/credentials.yml'))
 CONFIGS = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/../config/configuration.yml'))
 
+# Adding customized logging methods.
+# TODO: add proper block support. These methods are a performance penalty, even when the logging level is raised.
+class Logger
+	def d(msg)
+		puts "DEBUG #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 0)
+		debug msg
+	end
+	def i(msg)
+		puts "INFO #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 1)
+		info msg
+	end
+	def e(msg)
+		puts "ERROR #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 2)
+		error msg
+	end
+	def w(msg)
+		puts "WARN #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 3)
+		warn msg
+	end
+	def f(msg)
+		puts "FATAL #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 4)
+		fatal msg
+	end
+	def u(msg)
+		puts "#{msg}"
+		unknown msg
+	end
+end
+
 LOGGER = Logger.new ( File.expand_path(File.dirname(__FILE__) + '/../logs/' + CONFIGS[:logs][:filename], 'daily') )
 LOGGER.level = CONFIGS[:logs][:level].to_i
 LOGGER.datetime_format = '%Y-%m-%dT%H:%M:%S'
 
-aws_config = {
-  :access_key_id => CREDS[:aws][:access],
-  :secret_access_key => CREDS[:aws][:secret]
-}
-AWS.config(aws_config)
+AWS.config({:access_key_id => CREDS[:aws][:access], :secret_access_key => CREDS[:aws][:secret]})
 
 PARTY_CONFIG_MYSQL = Mysql.new(CREDS[:db][:party_config][:host], 
 								CREDS[:db][:party_config][:username], 
