760040a73e0274c6b43a3bc8c097a13226986b8e
Chris Cox <chriscox@abandonedfactory.net>
2013-10-24 22:46:02 -0700

    Adding support for requesting analysis

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,6 +1,33 @@
-require_relative '../src/PartyConfig'
+require_relative '../lib/PartyConfig'
+require_relative '../lib/config'
+require 'logger'
 
 class Manager
     attr_accessor :party
-end
 
+    def initialize
+      @party = PartyConfig.new
+      @logger = Logger.new('../logs/manager.log', 'daily')
+    end
+
+    def request_analysis
+      queue = CONFIGS[:aws][:sqs][:queue]
+      @logger.d("Requesting analysis via SQS queue #{queue}.")
+      sqs = AWS::SQS.new
+
+      q = sqs.queues['queue']
+      unless(q.exists?)
+        @logger.i ("Creating queue: #{queue}")
+        begin
+          q = sqs.queues.create(queue)
+        rescue AWS::SQS::Errors::InvalidParameterValue => e
+          @logger.f ("Invalid queue name '#{queue}'. Aborting!")
+          exit(1)
+        end
+      end
+
+      # Actually send the message
+      m = q.send_message("Analysis, please! PartyId=#{@party.id}")
+      @logger.d ("Message sent, MessageId=#{m.id}")
+    end
+end
