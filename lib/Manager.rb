59c9b7dca2aa91e7bf51695f43caca00cec64336
Chris Cox <chriscox@abandonedfactory.net>
2013-10-24 22:47:30 -0700

    Correcting indentation

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -3,31 +3,31 @@ require_relative '../lib/config'
 require 'logger'
 
 class Manager
-    attr_accessor :party
+  attr_accessor :party
 
-    def initialize
-      @party = PartyConfig.new
-      @logger = Logger.new('../logs/manager.log', 'daily')
-    end
+  def initialize
+    @party = PartyConfig.new
+    @logger = Logger.new('../logs/manager.log', 'daily')
+  end
 
-    def request_analysis
-      queue = CONFIGS[:aws][:sqs][:queue]
-      @logger.d("Requesting analysis via SQS queue #{queue}.")
-      sqs = AWS::SQS.new
+  def request_analysis
+    queue = CONFIGS[:aws][:sqs][:queue]
+    @logger.d("Requesting analysis via SQS queue #{queue}.")
+    sqs = AWS::SQS.new
 
-      q = sqs.queues['queue']
-      unless(q.exists?)
-        @logger.i ("Creating queue: #{queue}")
-        begin
-          q = sqs.queues.create(queue)
-        rescue AWS::SQS::Errors::InvalidParameterValue => e
-          @logger.f ("Invalid queue name '#{queue}'. Aborting!")
-          exit(1)
-        end
+    q = sqs.queues['queue']
+    unless(q.exists?)
+      @logger.i ("Creating queue: #{queue}")
+      begin
+        q = sqs.queues.create(queue)
+      rescue AWS::SQS::Errors::InvalidParameterValue => e
+        @logger.f ("Invalid queue name '#{queue}'. Aborting!")
+        exit(1)
       end
-
-      # Actually send the message
-      m = q.send_message("Analysis, please! PartyId=#{@party.id}")
-      @logger.d ("Message sent, MessageId=#{m.id}")
     end
+
+    # Actually send the message
+    m = q.send_message("Analysis, please! PartyId=#{@party.id}")
+    @logger.d ("Message sent, MessageId=#{m.id}")
+  end
 end
