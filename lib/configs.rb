fdc2d1afc51b38367fed4ef142a88b1b1489fb78
Chris Cox <chriscox@abandonedfactory.net>
2013-11-08 22:46:10 -0800

    Switching PartyConfig storage to DynamoDB

--- lib/configs.rb
+++ lib/configs.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 require 'logger'
 require 'date'
-require 'mysql'
+# require 'mysql'
 require 'rubygems'
 require 'aws-sdk'
 
@@ -11,36 +11,36 @@ CONFIGS = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/../config/c
 # Adding customized logging methods.
 # TODO: add proper block support. These methods are a performance penalty, even when the logging level is raised.
 class Logger
-	def d(msg=nil)
-		s = if (block_given? && CONFIGS[:logs][:level] >= 0)
-			yield
-		else
-			msg
-		end
+  def d(msg=nil)
+    s = if (block_given? && CONFIGS[:logs][:level] >= 0)
+      yield
+    else
+      msg
+    end
 
-		puts "DEBUG #{s}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 0)
-		debug msg
-	end
-	def i(msg)
-		puts " INFO #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 1)
-		info msg
-	end
-	def e(msg)
-		puts "ERROR #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 2)
-		error msg
-	end
-	def w(msg)
-		puts " WARN #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 3)
-		warn msg
-	end
-	def f(msg)
-		puts "FATAL #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 4)
-		fatal msg
-	end
-	def u(msg)
-		puts "  ANY #{msg}"
-		unknown msg
-	end
+    puts "DEBUG #{s}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 0)
+    debug msg
+  end
+  def i(msg)
+    puts " INFO #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 1)
+    info msg
+  end
+  def e(msg)
+    puts "ERROR #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 2)
+    error msg
+  end
+  def w(msg)
+    puts " WARN #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 3)
+    warn msg
+  end
+  def f(msg)
+    puts "FATAL #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 4)
+    fatal msg
+  end
+  def u(msg)
+    puts "  ANY #{msg}"
+    unknown msg
+  end
 end
 
 LOGGER = Logger.new ( File.expand_path(File.dirname(__FILE__) + '/../logs/' + CONFIGS[:logs][:filename], 'daily') )
@@ -49,8 +49,8 @@ LOGGER.datetime_format = '%Y-%m-%dT%H:%M:%S'
 
 AWS.config({:access_key_id => CREDS[:aws][:access_key], :secret_access_key => CREDS[:aws][:secret]})
 
-PARTY_CONFIG_MYSQL = Mysql.new(CREDS[:db][:party_config][:host], 
-								CREDS[:db][:party_config][:username], 
-								CREDS[:db][:party_config][:password], 
-								CREDS[:db][:party_config][:schema])
+# PARTY_CONFIG_MYSQL = Mysql.new(CREDS[:db][:party_config][:host], 
+#                 CREDS[:db][:party_config][:username], 
+#                 CREDS[:db][:party_config][:password], 
+#                 CREDS[:db][:party_config][:schema])
 
--- test/tc_partyconfig.rb
+++ test/tc_partyconfig.rb
@@ -2,74 +2,81 @@ require 'test/unit'
 require '../lib/PartyConfig'
 
 class TestPartyConfig < Test::Unit::TestCase
-  def test_add_member
-    conf = PartyConfig.new( [ 'Klaital', 'Demandred'] )
-
-    assert_equal(2, conf.player_characters.length)
-    assert(conf.player_characters.include?('Klaital'))
-    assert(conf.player_characters.include?('Demandred'))
-    assert(!conf.player_characters.include?('Nimbex'))
-
-    conf.add_player('Nimbex')
-    assert_equal(3, conf.player_characters.length)
-    assert(conf.player_characters.include?('Klaital'))
-    assert(conf.player_characters.include?('Demandred'))
-    assert(conf.player_characters.include?('Nimbex'))
+  def test_create_new_party
+    table = db.tables[CREDS[:db][:party_configs][:schema]]
+    
+    item = table.items.create('id' => 1, 'player_characters' => ['Klaital', 'Demandred'])
+    
   end
 
-  def test_load
-    conf = PartyConfig.load(1)
+#   def test_add_member
+#     conf = PartyConfig.new( [ 'Klaital', 'Demandred'] )
 
-    assert_equal(2, conf.player_characters.length)
-    assert(conf.player_characters.include?('Klaital'))
-    assert(conf.player_characters.include?('Demandred'))
-    assert(!conf.player_characters.include?('Nimbex'))
+#     assert_equal(2, conf.player_characters.length)
+#     assert(conf.player_characters.include?('Klaital'))
+#     assert(conf.player_characters.include?('Demandred'))
+#     assert(!conf.player_characters.include?('Nimbex'))
 
-    assert_equal('test party', conf.name)
-    assert_equal('2013-10-20 18:20:10', conf.start_time)
-    assert_equal('2013-10-20 19:20:17', conf.end_time)
-  end
+#     conf.add_player('Nimbex')
+#     assert_equal(3, conf.player_characters.length)
+#     assert(conf.player_characters.include?('Klaital'))
+#     assert(conf.player_characters.include?('Demandred'))
+#     assert(conf.player_characters.include?('Nimbex'))
+#   end
 
-  def test_basic_save
-    conf = PartyConfig.new(['Klaital', 'Demandred', 'Nimbex'])
-    conf.name = "test_basic_save config"
-    assert_nil(conf.id)
-    conf.save
+#   def test_load
+#     conf = PartyConfig.load(1)
 
-    assert_not_nil(conf.id)
-    id = conf.id
+#     assert_equal(2, conf.player_characters.length)
+#     assert(conf.player_characters.include?('Klaital'))
+#     assert(conf.player_characters.include?('Demandred'))
+#     assert(!conf.player_characters.include?('Nimbex'))
 
-    conf2 = PartyConfig.load(id)
-    assert_equal(3, conf.player_characters.length)
-    assert_equal("test_basic_save config", conf2.name)
-    assert(conf2.player_characters.include?('Klaital'))
-    assert(conf2.player_characters.include?('Demandred'))
-    assert(conf2.player_characters.include?('Nimbex'))
-    conf2.player_characters << 'Morlock'
-    conf2.save
-    assert_equal(id, conf2.id)
+#     assert_equal('test party', conf.name)
+#     assert_equal('2013-10-20 18:20:10', conf.start_time)
+#     assert_equal('2013-10-20 19:20:17', conf.end_time)
+#   end
 
-    conf3 = PartyConfig.load(id)
-    assert_equal(4, conf3.player_characters.length)
-    assert(conf3.player_characters.include?('Klaital'))
-    assert(conf3.player_characters.include?('Demandred'))
-    assert(conf3.player_characters.include?('Nimbex'))
-    assert(conf3.player_characters.include?('Morlock'))
+#   def test_basic_save
+#     conf = PartyConfig.new(['Klaital', 'Demandred', 'Nimbex'])
+#     conf.name = "test_basic_save config"
+#     assert_nil(conf.id)
+#     conf.save
 
-    conf3.delete
-  end
+#     assert_not_nil(conf.id)
+#     id = conf.id
 
-  def test_delete
-    conf = PartyConfig.new(['Klaital', 'Demandred', 'Nimbex'])
-    conf.name = "test_basic_save config"
-    assert_nil(conf.id)
-    conf.save
-    id = conf.id
-    assert_not_nil(conf.id)
-    conf.delete
+#     conf2 = PartyConfig.load(id)
+#     assert_equal(3, conf.player_characters.length)
+#     assert_equal("test_basic_save config", conf2.name)
+#     assert(conf2.player_characters.include?('Klaital'))
+#     assert(conf2.player_characters.include?('Demandred'))
+#     assert(conf2.player_characters.include?('Nimbex'))
+#     conf2.player_characters << 'Morlock'
+#     conf2.save
+#     assert_equal(id, conf2.id)
 
-    assert_nil(conf.id)
-    conf2 = PartyConfig.load(id)
-    assert(!conf2)
-  end
+#     conf3 = PartyConfig.load(id)
+#     assert_equal(4, conf3.player_characters.length)
+#     assert(conf3.player_characters.include?('Klaital'))
+#     assert(conf3.player_characters.include?('Demandred'))
+#     assert(conf3.player_characters.include?('Nimbex'))
+#     assert(conf3.player_characters.include?('Morlock'))
+
+#     conf3.delete
+#   end
+
+#   def test_delete
+#     conf = PartyConfig.new(['Klaital', 'Demandred', 'Nimbex'])
+#     conf.name = "test_basic_save config"
+#     assert_nil(conf.id)
+#     conf.save
+#     id = conf.id
+#     assert_not_nil(conf.id)
+#     conf.delete
+
+#     assert_nil(conf.id)
+#     conf2 = PartyConfig.load(id)
+#     assert(!conf2)
+#   end
 end
