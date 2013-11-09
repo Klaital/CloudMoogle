f744ff8d0f50d1924694fda4dc72ce726f643cae
Chris Cox <chriscox@abandonedfactory.net>
2013-11-09 08:29:33 -0800

    Adding custom logger methods

--- lib/configs.rb
+++ lib/configs.rb
@@ -8,39 +8,69 @@ require 'aws-sdk'
 CREDS = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/../config/credentials.yaml'))
 CONFIGS = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/../config/configuration.yaml'))
 
-# Adding customized logging methods.
-# TODO: add proper block support. These methods are a performance penalty, even when the logging level is raised.
+# Adding customized logging methods. 
+# These check out the app config for the log level and optionally whether to echo onto stdout as well.
 class Logger
-  def d(msg=nil)
-    s = if (block_given? && CONFIGS[:logs][:level] >= 0)
-      yield
-    else
-      msg
-    end
+	def d(msg=nil)
+		s = if (block_given? && CONFIGS[:logs][:level] >= 0)
+			yield
+		else
+			msg
+		end
 
-    puts "DEBUG #{s}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 0)
-    debug msg
-  end
-  def i(msg)
-    puts " INFO #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 1)
-    info msg
-  end
-  def e(msg)
-    puts "ERROR #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 2)
-    error msg
-  end
-  def w(msg)
-    puts " WARN #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 3)
-    warn msg
-  end
-  def f(msg)
-    puts "FATAL #{msg}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 4)
-    fatal msg
-  end
-  def u(msg)
-    puts "  ANY #{msg}"
-    unknown msg
-  end
+		puts "DEBUG #{s}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 0)
+		debug s
+	end
+	def i(msg=nil)
+		s = if (block_given? && CONFIGS[:logs][:level] >= 1)
+			yield
+		else
+			msg
+		end
+
+		puts " INFO #{s}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 1)
+		info s
+	end
+	def e(msg=nil)
+		s = if (block_given? && CONFIGS[:logs][:level] >= 2)
+			yield
+		else
+			msg
+		end
+
+		puts "ERROR #{s}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 2)
+		error s
+	end
+	def w(msg=nil)
+		s = if (block_given? && CONFIGS[:logs][:level] >= 3)
+			yield
+		else
+			msg
+		end
+
+		puts " WARN #{s}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 3)
+		warn s
+	end
+	def f(msg=nil)
+		s = if (block_given? && CONFIGS[:logs][:level] >= 4)
+			yield
+		else
+			msg
+		end
+
+		puts "FATAL #{s}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 4)
+		fatal s
+	end
+	def u(msg)
+		s = if (block_given? && CONFIGS[:logs][:stdout])
+			yield
+		else
+			msg
+		end
+
+		puts "  ANY #{s}" if (CONFIGS[:logs][:stdout] && CONFIGS[:logs][:level] >= 3)
+		unknown s
+	end
 end
 
 LOGGER = Logger.new ( File.expand_path(File.dirname(__FILE__) + '/../logs/' + CONFIGS[:logs][:filename], 'daily') )
