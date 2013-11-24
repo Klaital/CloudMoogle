d655f5b6db5f3c66f150455303a5349697d59dad
Chris Cox <chriscox@abandonedfactory.net>
2013-11-24 15:23:17 -0800

    Updating indentation

--- lib/Weaponskills.rb
+++ lib/Weaponskills.rb
@@ -1,22 +1,21 @@
 # add knowledge of Weaponskill names to the Patterns class
 
 class Patterns
-    def Patterns.weaponskills(load_path = "../resources/weaponskills.txt")
-        if(File.exists?(load_path))
-            f = File.open(load_path, "r")
-            ret = f.readlines.collect {|x| x.strip}
-            f.close
-            return ret
-        end
-        return []
+  def Patterns.weaponskills(load_path = "../resources/weaponskills.txt")
+    if(File.exists?(load_path))
+      f = File.open(load_path, "r")
+      ret = f.readlines.collect {|x| x.strip}
+      f.close
+      return ret
     end
-    
-    def Patterns.attack_jas
-        [ 'Weapon Bash', 'Blade Bash', 'Shield Bash', 'Violent Flourish', 
-            '(Fire|Ice|Wind|Earth|Thunder|Water) Shot',
-            '(High |Super |Spirit |Soul )?Jump', 
-            'Chi Blast', 'Mijin Gakure', 'Eagle Eye Shot', 'Barrage' 
-            ]
-    end
-    
+    return []
+  end
+
+  def Patterns.attack_jas
+    [ 'Weapon Bash', 'Blade Bash', 'Shield Bash', 'Violent Flourish', 
+      '(Fire|Ice|Wind|Earth|Thunder|Water) Shot',
+      '(High |Super |Spirit |Soul )?Jump', 
+      'Chi Blast', 'Mijin Gakure', 'Eagle Eye Shot', 'Barrage' 
+    ]
+  end
 end
