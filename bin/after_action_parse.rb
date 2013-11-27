dbee2dfa3e0daad5a5815ddd3f9fd86cfed3d862
Chris Cox <chriscox@abandonedfactory.net>
2013-11-26 17:34:00 -0800

    Switching to emitting an XML document for now, until better renderers are written

--- bin/after_action_parse.rb
+++ bin/after_action_parse.rb
@@ -4,13 +4,19 @@
 # Usage: ruby after_action_parse.rb PATH_TO_LOGFILE
 
 require_relative '../lib/Parser'
+require_relative '../lib/Analyzer'
 unless (ARGV.length > 0 && File.exist?(ARGV[0]))
     puts "Please specify the logfile to parse"
     puts "Usage: ruby after_action_parse.rb PATH_TO_LOGFILE"
     exit
 end
 
+player_characters = ['Klaital', 'Demandred', 'Cydori', 'Elizara', 'Nimbex', 'Morlock', 'Kireila', 'Drydin', 'Beanies', 'Tharpy', 'Neresh', 'Wrex']
+
 p = Parser.new
 p.parse_file(ARGV[0])
 puts "#{p.actions.length} actions parsed"
+a = Analyzer.new
+puts a.analyze_offense(p.actions, player_characters)
+
 
