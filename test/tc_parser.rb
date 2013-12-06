c4dade2671cf897214ddda93697080534321e4ff
Chris Cox <chriscox@abandonedfactory.net>
2013-12-06 12:39:45 -0800

    Correctly skip blank lines without interrupting multiline actions

--- test/tc_parser.rb
+++ test/tc_parser.rb
@@ -16,6 +16,34 @@ class TestParser < Test::Unit::TestCase
     assert_equal('Colibri', a.target)
     assert_equal(4, a.damage)
   end
+  
+  def test_parse_crits
+	a = Parser.parse_line('[18:32:27]Nimbex scores a critical hit!', nil)
+	a = Parser.parse_line('[18:32:27]The Sharabha takes 136 points of damage.', a)
+	assert_not_nil(a)
+	assert_equal('MELEE', a.type)
+    assert_equal('CRIT', a.subtype)
+    assert_equal('COMBAT', a.format)
+    assert_equal('MELEE', a.ability_name)
+    assert_equal('Nimbex', a.actor)
+    assert_equal('Sharabha', a.target)
+    assert_equal(136, a.damage)
+  end
+  
+  def test_parse_crits_from_file
+	p = Parser.new
+	p.parse_file(File.join(File.dirname(__FILE__), 'faked_test_logs', 'single_crit.log'))
+	assert_equal(1, p.actions.length)
+	a = p.actions[0]
+	assert_not_nil(a)
+	assert_equal('MELEE', a.type)
+    assert_equal('CRIT', a.subtype)
+    assert_equal('COMBAT', a.format)
+    assert_equal('MELEE', a.ability_name)
+    assert_equal('Drydin', a.actor)
+    assert_equal('Sharabha', a.target)
+    assert_equal(326, a.damage)
+  end
 
   def test_parse_file
     @p.parse_file(File.join('faked_test_logs', 'hills1.log'))
