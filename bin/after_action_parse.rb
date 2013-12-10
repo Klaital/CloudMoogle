de5a908965f08710f1e00e4647d7c44ac5b91865
Chris Cox <chriscox@abandonedfactory.net>
2013-12-09 18:20:05 -0800

    Fleshed out after-action analysis

--- bin/after_action_parse.rb
+++ bin/after_action_parse.rb
@@ -1,12 +1,28 @@
 # Used to re-parse a logfile after it's been closed. This is useful for generating a final after-action report
 # for debriefing the party. 
 # 
-# Usage: ruby after_action_parse.rb PATH_TO_LOGFILE
+# Usage: ruby after_action_parse.rb -l PATH_TO_LOGFILE [-p PARTY_ID]
 
 require_relative '../lib/Manager.rb'
 
-party_id = (ARGV.length < 1) ? 'bae0f118-798d-40d5-a66d-dda40e9eddd6' : ARGV[-1] 
+def usage
+  s = <<USAGE
+> ruby #{__FILE__} -l PATH_TO_LOGFILE [-p PARTY_ID] [-s SLEEP_SECS]
+USAGE
+
+  return s
+end
+party_id = parse_arg(ARGV, ['-p','--party'], true, 'bae0f118-798d-40d5-a66d-dda40e9eddd6')
+log_path = parse_arg(ARGV, ['-l','--logfile'], true, nil)
+sleep_interval = parse_arg(ARGV, ['-s', '--sleep'], true, 10)
+
+if (log_path.nil? || !File.exists?(log_path))
+  puts usage
+  exit 1
+end
+
 m = Manager.new(party_id)
-m.sleep_interval = 10
-path = File.join(File.dirname(__FILE__), 'sample_logs', 'Klaital_2011.04.04-Sharabha1.log')
-m.run!(path)
+m.sleep_interval = sleep_interval
+File.open(log_path, 'r') do |f|
+  m.run!(f)
+end
