2a4f68e299e1be70f6b06bb9fd0181a74281cda6
Chris Cox <chriscox@abandonedfactory.net>
2013-12-11 12:57:38 -0800

    Making variable names more specific

--- test/tc_analyzer.rb
+++ test/tc_analyzer.rb
@@ -6,14 +6,20 @@ require_relative '../lib/Analyzer'
 
 class TestAnalyzer < Test::Unit::TestCase
   def setup
-    @p = Parser.new
-    @p.parse_file(File.join(File.dirname(__FILE__), 'faked_test_logs', 'sharabha1.log'))
+    @sharabha_parse = Parser.new
+    @sharabha_parse.parse_file(File.join(File.dirname(__FILE__), 'faked_test_logs', 'sharabha1.log'))
     @a = Analyzer.new
-    @party = PartyConfig.new(['Klaital','Kireila', 'Demandred', 'Drydin', 'Nimbex', 'Morlock'], Time.parse('2011-04-04T18:32:00'), Time.parse('2011-04-04T19:32:00'))
+    @sharabha_party = PartyConfig.new
+    @sharabha_party.player_characters = ['Klaital','Kireila', 'Demandred', 'Drydin', 'Nimbex', 'Morlock']
+    @sharabha_party.start_time = Time.parse('2011-04-04T18:32:00')
+    @sharabha_party.end_time   = Time.parse('2011-04-04T19:32:00')
   end
   
   def test_player_analysis
-    data = @a.analyze_by_players(@p.actions, @party.player_characters)
+    assert_not_nil(@sharabha_parse)
+    assert(@sharabha_parse.actions.length > 0, "No actions parsed for Sharabha run")
+    assert(@sharabha_party.player_characters.length, "No party members found for Sharabha run")
+    data = @a.analyze_by_players(@sharabha_parse.actions, @sharabha_party.player_characters)
     assert(data.kind_of?(Hash), "#analyze_by_players did not return a data hash!")
     assert_equal(2, data['Klaital'][:damage_dealt_total])
     assert_equal(480, data['Nimbex'][:damage_dealt_total])
